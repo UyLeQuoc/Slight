9db3414c385f0206c4d77d69b16ca2115363d3d5
MarkVo_Mac <markvo_mac@MarkVo-Macs-MacBook-Pro.local>
2023-05-28 16:56:34 +0700

    Fix (Homepage): added key to guideSection

--- src/components/GuideSection.jsx
+++ src/components/GuideSection.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 import Step from '@/components/Step'
 
 const content = [
-    {'title': 'STEP 1: Start by Adding Your Text',
+    { 'id': 1,
+      'title': 'STEP 1: Start by Adding Your Text',
       'description': 'Enter the text that you want to create your presentation from. You can either write it directly into the text box, paste it in from any source or upload your document under formats .DOC, .DOCX, .PDF',
       'titleOnLeftSide': true,
       'imgSrc': '/step1.svg',
     },
-      {'title': 'STEP 2: Customise the look and feel',
+      {'id': 2,
+      'title': 'STEP 2: Customise the look and feel',
       'description': 'Fill out the form which help our AI to figure out your style and create the slide to match it.',
       'titleOnLeftSide': false,
       'imgSrc': '/step2.svg',
     },
-      {'title': 'STEP 3: Generate your slides',
+      {'id': 3,
+      'title': 'STEP 3: Generate your slides',
       'description': 'Take a moment to preview your presentation and make any final adjustments.',
       'titleOnLeftSide': true,
       'imgSrc': '/step3.svg',
     },
-      {'title': 'STEP 4: Customise YOUR SLIDE',
+      {'id': 4,
+      'title': 'STEP 4: Customise YOUR SLIDE',
       'description': 'Tùy ý chính sửa Silde của mình, tùy thuộc vào loại account của bạn mà sẽ có giới hạn khác nhau.',
       'titleOnLeftSide': false,
       'imgSrc': '/step4.svg',
@@ -32,7 +36,7 @@ function GuideSection() {
       <div className='guide-stepList'>
         {
           content.map((o) => (
-            <Step title={o.title} description={o.description} titleOnLeftSide={o.titleOnLeftSide} imgSrc={o.imgSrc}></Step>
+            <Step key={o.id} title={o.title} description={o.description} titleOnLeftSide={o.titleOnLeftSide} imgSrc={o.imgSrc}></Step>
         ))}
       </div>
     </div>
