4657b6f1ed7cbdc68682ebd4bfe89eced9b0b066
MarkVo_Mac <markvo_mac@MarkVo-Macs-MacBook-Pro.local>
2023-06-01 16:27:25 +0700

    update scrolling on navbar

---
+++ src/pages/create.jsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/common/Navbar'
+import React from 'react'
+
+function create() {
+  return (
+    <>
+      <Navbar></Navbar>
+      
+      <PromotionSection></PromotionSection>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default create
