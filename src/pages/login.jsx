510bae3a1a9a1265733968b5676f10b6b138a0c3
MarkVo_Mac <markvo_mac@192.168.2.8>
2023-06-06 22:58:19 +0700

    Added Login Signup pages

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,16 +1,66 @@
-import LoginForm from '@/components/LoginForm'
 import React from 'react'
 import { withPublic } from "@/hooks/route";
-import { Button } from 'antd';
+import { Button, notification } from 'antd';
+import Navbar from '@/components/common/Navbar';
+import Footer from '@/components/common/Footer';
+import PromotionSection from '@/components/PromotionSection';
+import Link from 'next/link';
 
 function LoginPage({auth}) {
   const { user, loginWithGoogle, error } = auth;
+	if (error!= null) {
+		notification.error({message: 'Login failed', description: error });
+	}
 	return (
-		<div>
-			{error && <h1>{error}</h1>}
-			<Button onClick={loginWithGoogle}>Google</Button>
-			<h1>{user?.uid}</h1>
-		</div>
+		<>
+			<Navbar></Navbar>
+			<div className="login-form-wrapper flex flex-row-reverse">
+				<div className="bubble red">
+					<img src="/bubble1.svg" alt="decoration bubble" />
+				</div>
+				<div className="bubble blue">
+					<img src="/bubble3.svg" alt="decoration bubble" />
+				</div>
+				<div className="bubble gray">
+					<img src="/bubble2.svg" alt="decoration bubble" />
+				</div>
+				<div className="login-form-container flex flex-row-reverse justify-end">
+					<div className="login-form">
+						<div className='title-contianer'>
+							<div className='title'>Log in</div>
+						</div>
+						<form >
+							<div className='input-container flex flex-col justify-between'>
+								<input type="email" className="username" placeholder="@Email"></input>
+								<input type="password" className="password" placeholder="Password"></input>
+							</div>
+							
+							<div className="signup-link text-right mx-[2rem]">New user?<Link href="/signup"> <span>Create an account</span></Link></div>
+							<div className='login-with-creds-btn-container flex flex-row justify-center'>
+								<Button className='login-with-creds-btn'>Continue</Button>
+							</div>
+							<div className='divider-container flex flex-row justify-center'>
+								<div className="divider">or</div>
+							</div>
+							
+							<div className="third-party-login-btn-container flex flex-row justify-center">
+								<div onClick={loginWithGoogle} className='icon'>
+									<img src='/btn_google_light_normal_ios.svg'></img>
+								</div>
+								<div className='icon'>
+									<img src='/facebook-icon.svg'></img>
+								</div>
+								<div className='icon'>
+									<img src='/twitter-icon.svg'></img>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<PromotionSection></PromotionSection>
+			<Footer></Footer>
+		</>
 	);
 }
 
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { withPublic } from "@/hooks/route";
+import { Button, notification } from 'antd';
+import Navbar from '@/components/common/Navbar';
+import Footer from '@/components/common/Footer';
+import PromotionSection from '@/components/PromotionSection';
 
 function SignUpPage() {
   return (
-    <div>SignUpPage</div>
+    <>
+    <Navbar></Navbar>
+    <div className="signup-form-wrapper flex flex-row justify-center">
+      <div className="signup-form-container flex flex-row justify-end">
+        <div className="signup-form">
+          <div className='title-contianer'>
+            <div className='title'>Sign up</div>
+          </div>
+          <form>
+            <div className='input-container flex flex-col justify-between'>
+              <input type="text" className="name" placeholder="Name"></input>
+              <input type="email" className="username" placeholder="@Email"></input>
+              <input type="password" className="password" placeholder="Password"></input>
+            </div>
+              <div className='signup-with-creds-btn-container flex flex-row justify-center'>
+              <div className='signup-with-creds-btn'>Continue</div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <PromotionSection></PromotionSection>
+    <Footer></Footer>
+  </>
   )
 }
 
