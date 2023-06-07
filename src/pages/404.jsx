9a820fe0eba795aa198fe74db28380192083bfd0
Mark Vo <62336255+MarkVoVN@users.noreply.github.com>
2023-06-07 14:34:42 +0700

    Added 404 403 pages (#9)

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,46 @@
+import Footer from '@/components/common/Footer'
+import Navbar from '@/components/common/Navbar'
+import { Button } from 'antd'
 import React from 'react'
+import Link from 'next/link'
 
 function pageNotFound() {
+  const data = {
+    type: '404',
+    imgUrl: '/page-not-found.svg',
+    title: 'Oops..Page Not Found',
+    subtitle: 'he page you are looking for doesn&apos;t seem to exist.',
+    buttons: [
+      {name: 'HOME',
+      href: '/'}
+    ]
+
+  }
+
   return (
-    <div>404</div>
+    <>
+      <Navbar></Navbar>
+      <div className="message-wrapper flex flex-row justify-center">
+        <div className='message-container flex flex-col items-center'>
+            <img className='w-[40vw]' src={data.imgUrl} alt={data.title} />
+            <div className='title'>
+              {data.title}
+            </div>
+            <div className='subtitle'>
+              {data.subtitle}
+            </div>
+            <div className='btn-container flex flex-row justify-center'>
+              {data.buttons.map((btn) => (
+                <Button className='btn' size='large' key={btn.name}>
+                <Link href={btn.href}>{btn.name}</Link>
+                </Button>
+              ))}
+              
+            </div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
   )
 }
 
