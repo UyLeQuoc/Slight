7d3fe2bd4c886333260d2f430bb57d28a39dc06e
Vo Minh Khoi <vominhkhoi12032003@gmail.com>
2023-06-09 19:41:17 +0700

    hot fix

--- src/components/create/Step1.jsx
+++ src/components/create/Step1.jsx
@@ -7,7 +7,7 @@ function Step1({nextStep, topic, setTopic, message}) {
             <div className='step-title '>STEP 1</div>
             <div className='step-tips text-lg'>
             Enter your topic, a detailed description of 10-15 words will be perfect.  
-        Don't have a topic? Try one of these: 'Albert Enstein', 'Cristiano Ronaldo' 
+        Don't have a topic? Try one of these: 'Albert Einstein', 'Cristiano Ronaldo' 
             </div>
             {message && <div className='message-container mt-1rem text-center'>{'ERROR: ' + message}</div>}
             <div className='step-input-container flex flex-col justify-center'>
